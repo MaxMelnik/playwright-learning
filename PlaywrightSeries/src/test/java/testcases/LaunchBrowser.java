2d83625ed25bf0164c8d164536ae414ef118bc58
Maks Melnyk <maks.melnik1611@gmail.com>
2025-09-20 19:46:56 +0300

    implement lesson 2.13. Maximize window

--- PlaywrightSeries/src/test/java/testcases/LaunchBrowser.java
+++ PlaywrightSeries/src/test/java/testcases/LaunchBrowser.java
@@ -1,5 +1,8 @@
 package testcases;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import com.microsoft.playwright.*;
 
 public class LaunchBrowser {
@@ -7,14 +10,22 @@ public class LaunchBrowser {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Started [");
         //try (Playwright playwright = Playwright.create()) {
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		
+		System.out.println(width+":"+height);
+		
 		Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
+        BrowserContext browserContext = browser.newContext(new Browser.NewContextOptions().setViewportSize((int)width, (int)height));
+        Page page = browserContext.newPage();
         page.navigate("https://playwright.dev");
         System.out.println(page.title());
         Thread.sleep(2000);
-        page.close();
-        playwright.close();
+//        page.close();
+//        playwright.close();
         //}
 		System.out.println("] Ended");
 	}
