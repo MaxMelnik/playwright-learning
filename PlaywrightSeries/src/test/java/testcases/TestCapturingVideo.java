2fb0c313d7f3c5edc1872b0644fe96b3334346d6
Maks Melnyk <maks.melnik1611@gmail.com>
2025-09-24 18:19:03 +0300

    implement lesson 10.67. Capturing Video

---
+++ PlaywrightSeries/src/test/java/testcases/TestCapturingVideo.java
@@ -0,0 +1,66 @@
+package testcases;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.SelectOption;
+
+public class TestCapturingVideo {
+
+	public static void main(String[] args) {
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.chromium()
+				.launch(
+						new BrowserType
+							.LaunchOptions()
+							.setHeadless(false));
+		
+		BrowserContext context = browser.newContext(
+				new Browser.NewContextOptions()
+				.setRecordVideoDir(Paths.get("videos/"))
+				.setScreenSize(800, 600));	 
+		
+		Page page = context.newPage();
+		page.navigate("https://www.wikipedia.org");
+
+		// select by value
+		page.selectOption("select", "sv");
+		
+		//select by text
+		page.selectOption("select", new SelectOption().setLabel("Eesti"));
+
+		//select by index
+		page.selectOption("select", new SelectOption().setIndex(1));
+		
+		
+		/*Locator values = page.locator("select > option");
+		System.out.println(values.count());
+		
+		
+		for (int i=0; i<values.count(); i++) {
+			String text = values.nth(i).innerText();
+			String lang = values.nth(i).getAttribute("lang");
+			System.out.println(i + ". " + text + ": " + lang);
+		}*/
+		
+		List <ElementHandle> values = page.querySelectorAll("select > option");
+		
+		System.out.println(values.size());
+		
+		for (ElementHandle value: values) {
+			String text = value.innerText();
+			String lang = value.getAttribute("lang");
+			System.out.println(text + ": " + lang);
+		}
+		
+		page.close();
+		context.close();
+		playwright.close();
+	}
+}
