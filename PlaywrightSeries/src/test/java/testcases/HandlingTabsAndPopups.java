8daab790f81c88fb38fe19e0ed6f81e2292e2ec0
Maks Melnyk <maks.melnik1611@gmail.com>
2025-09-24 15:12:11 +0300

    implement lesson 5.42. Handling Tabs

---
+++ PlaywrightSeries/src/test/java/testcases/HandlingTabsAndPopups.java
@@ -0,0 +1,36 @@
+package testcases;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Locator.PressSequentiallyOptions;
+
+public class HandlingTabsAndPopups {
+
+	public static void main(String[] args) throws InterruptedException {
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.chromium()
+				.launch(
+						new BrowserType
+							.LaunchOptions()
+							.setHeadless(false));
+ 
+		Page page = browser.newPage();
+		page.navigate("https://sso.teachable.com/secure/673/identity/sign_up/email");
+	
+		
+		Page popup = page.waitForPopup(() -> {
+			page.locator("text=Privacy Policy").nth(0).click();
+		});
+		
+		popup.locator("//*[@id=\"header-sign-up-btn\"]").click();
+		Thread.sleep(5000);
+		popup.locator("#user_name")
+			.pressSequentially(
+					"Maks Melnyk", 
+					new PressSequentiallyOptions()
+					.setDelay(100));
+	}
+
+}
