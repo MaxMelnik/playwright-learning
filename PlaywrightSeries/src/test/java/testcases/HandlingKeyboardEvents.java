a1e9ae2eae61e0f9cd49ae6323ef828fbd0fae30
Maks Melnyk <maks.melnik1611@gmail.com>
2025-09-24 15:58:08 +0300

    implement lesson 7.56. Handling Keyboard Events

---
+++ PlaywrightSeries/src/test/java/testcases/HandlingKeyboardEvents.java
@@ -0,0 +1,48 @@
+package testcases;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Locator.PressSequentiallyOptions;
+
+public class HandlingKeyboardEvents {
+
+	public static void main(String[] args) throws InterruptedException {
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.chromium()
+				.launch(
+						new BrowserType
+							.LaunchOptions()
+							.setHeadless(false));
+ 
+		Page page = browser.newPage();
+		page.navigate("https://login.yahoo.com/");
+		
+		page.locator("#login-username")
+				.pressSequentially(
+						"login@gmail.com",
+						new PressSequentiallyOptions()
+						.setDelay(100));
+		
+		page.keyboard().press("Enter");
+		Thread.sleep(1000);
+		page.goBack();
+		page.keyboard().press("Control+A");
+		Thread.sleep(1000);
+		page.keyboard().press("Control+X");
+		Thread.sleep(1000);
+		page.keyboard().press("Control+V");
+		Thread.sleep(1000);
+		page.keyboard().down("Shift");
+		
+		for (int i=0; i<3; i++) {
+			page.keyboard().press("ArrowLeft");
+			Thread.sleep(500);
+		}
+
+		page.keyboard().up("Shift");
+	
+	}
+
+}
