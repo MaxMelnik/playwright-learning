14afa1b055f25f8e1eafc17b205f3d137c7e89e6
Maks Melnyk <maks.melnik1611@gmail.com>
2025-09-24 15:25:14 +0300

    implement lesson 6.46. Handling Sliders

--- PlaywrightSeries/src/test/java/testcases/HandlingSlider.java
+++ PlaywrightSeries/src/test/java/testcases/HandlingSlider.java
@@ -1,10 +1,33 @@
 package testcases;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 public class HandlingSlider {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.chromium()
+				.launch(
+						new BrowserType
+							.LaunchOptions()
+							.setHeadless(false));
+ 
+		Page page = browser.newPage();
+		page.navigate("https://jqueryui.com/resources/demos/slider/default.html");
+	
+		Locator slider = page.locator("//*[@id=\"slider\"]/span");
+		Thread.sleep(2000);
+		
+		Double x = slider.boundingBox().x + slider.boundingBox().width/2;
+		Double y = slider.boundingBox().y + slider.boundingBox().height/2;
+		page.mouse().move(x, y);
+		page.mouse().down();
+		page.mouse().move(x + 400, y);
+		page.mouse().up();
 	}
 
 }
