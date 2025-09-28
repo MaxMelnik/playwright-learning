5da4c7e6073d382355a991cbe49f9a033ce4663c
Maks Melnyk <maks.melnik1611@gmail.com>
2025-09-28 14:04:24 +0300

    implement lesson 13.81. Log4j 1

--- PlaywrightSeries/src/test/java/testcases/TestProperties.java
+++ PlaywrightSeries/src/test/java/testcases/TestProperties.java
@@ -4,19 +4,26 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 public class TestProperties {
 
 	public static void main(String[] args) throws IOException {
 		
-		
+		Logger log = Logger.getLogger(TestProperties.class);
+		PropertyConfigurator.configure("./src/test/resources/properties/log4j.properties");
 		Properties prop = new Properties();
 		FileInputStream fis 
 		= new FileInputStream(
 				".\\src\\test\\resources\\properties\\locators.properties"
 				);
 		prop.load(fis);
+		log.info("Locator properties file loaded");
 		
 		System.out.println(prop.get("browser"));
+		log.info("Chrome browser launched");
+		log.info("test execution compleated");
 	}
 
 }
