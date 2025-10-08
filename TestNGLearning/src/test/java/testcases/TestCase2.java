1da9720b58badcee1a102a0c743631b4bc7f29ba
Maks Melnyk <maks.melnik1611@gmail.com>
2025-10-08 18:10:27 +0300

    implement lesson 14.93. Soft assertions

--- TestNGLearning/src/test/java/testcases/TestCase2.java
+++ TestNGLearning/src/test/java/testcases/TestCase2.java
@@ -2,12 +2,12 @@ package testcases;
 
 import org.testng.Assert;
 import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
 
 public class TestCase2 {
 
 	@Test
 	public void validateTitles() {
-		
 		String expected_title = "Yahoo.com";
 		String actual_title = "Gmail.com";
 		
@@ -22,5 +22,23 @@ public class TestCase2 {
 	@Test
 	public void assertFail() {
 		Assert.fail("Failing the test as the condition is not met");
+	}
+
+	@Test
+	public void softAssert() {
+		System.out.println("Start...");
+		
+		SoftAssert softAssert = new SoftAssert();
+		
+		String expected_title = "Yahoo.com";
+		String actual_title = "Gmail.com";
+		
+		softAssert.assertEquals(actual_title, expected_title);
+
+		softAssert.assertEquals(true, false, "soft fail");
+		
+		System.out.println("...Finish");
+		
+		softAssert.assertAll();
 	}
 }
