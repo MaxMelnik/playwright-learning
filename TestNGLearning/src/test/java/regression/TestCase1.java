aa6b44e62a4865e03a55210f2e923f23301ab08e
Maks Melnyk <maks.melnik1611@gmail.com>
2025-10-15 16:39:28 +0300

    implement lesson 14.96. Managing multiple suites

---
+++ TestNGLearning/src/test/java/regression/TestCase1.java
@@ -0,0 +1,36 @@
+package regression;
+
+import org.testng.annotations.*;
+
+public class TestCase1 {
+	
+	@BeforeTest
+	public void creatingDBConn() {
+		System.out.println("Creating db conn");
+	}
+	
+	@AfterTest
+	public void closingDBConn() {
+		System.out.println("Closing db conn");
+	}
+	
+	@BeforeMethod
+	public void launchBrowser() {
+		System.out.println("Launching browser");
+	}
+	
+	@AfterMethod
+	public void closeBrowser() {
+		System.out.println("Closing browser");
+	}
+	
+	@Test(priority=1)
+	public void doUserReg() {
+		System.out.println("Executing user registration test");
+	}
+
+	@Test(priority=2)
+	public void doLogin() {
+		System.out.println("Executing login test");
+	}
+}
---
+++ TestNGLearning/src/test/java/regression/TestCase2.java
@@ -0,0 +1,44 @@
+package regression;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
+public class TestCase2 {
+
+	@Test
+	public void validateTitles() {
+		String expected_title = "Yahoo.com";
+		String actual_title = "Gmail.com";
+		
+		Assert.assertEquals(actual_title, expected_title);
+	}
+	
+	@Test
+	public void validateElement() {
+		Assert.assertTrue(false, "Element not found");
+	}
+
+	@Test
+	public void assertFail() {
+		Assert.fail("Failing the test as the condition is not met");
+	}
+
+	@Test
+	public void softAssert() {
+		System.out.println("Start...");
+		
+		SoftAssert softAssert = new SoftAssert();
+		
+		String expected_title = "Yahoo.com";
+		String actual_title = "Gmail.com";
+		
+		softAssert.assertEquals(actual_title, expected_title);
+
+		softAssert.assertEquals(true, false, "soft fail");
+		
+		System.out.println("...Finish");
+		
+		softAssert.assertAll();
+	}
+}
