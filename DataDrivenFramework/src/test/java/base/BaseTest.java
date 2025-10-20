1ffee07db7bddfc518e92877c97602acbd5a286b
Maks Melnyk <maks.melnik1611@gmail.com>
2025-10-20 15:24:28 +0300

    implement lesson 17.118. LP1 - Data + Keyword driven Framework. Base Test

---
+++ DataDrivenFramework/src/test/java/base/BaseTest.java
@@ -0,0 +1,91 @@
+package base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class BaseTest {
+	
+	private Playwright playwright;
+	public Browser browser;
+	public Page page;
+	private static Properties OR = new Properties();
+	private static FileInputStream fis;
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	
+	@BeforeSuite
+	public void setUp() {
+		PropertyConfigurator.configure("./src/test/resorces/properties");
+		log.info("Test Execution started!");
+		
+		try {
+			fis = new FileInputStream("./src/test/resorces/properties/OR.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			OR.load(fis);
+			log.info("OR Properties file loaded.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public Browser getBrowser(String browserName) {
+		playwright = Playwright.create();
+		
+		switch(browserName) {
+		case "chrome":
+			log.info("Launching Chrome browser");
+			return playwright.chromium().launch(
+					new BrowserType.LaunchOptions()
+					.setChannel("chrome")
+					.setHeadless(false));
+		case "firefox":
+			log.info("Launching Firefox browser");
+			return playwright.firefox().launch(
+					new BrowserType.LaunchOptions()
+					.setChannel("firefox")
+					.setHeadless(false));
+		case "webkit":
+			log.info("Launching Webkit browser");
+			return playwright.webkit().launch(
+					new BrowserType.LaunchOptions()
+					.setHeadless(false));
+		case "headless":
+			log.info("Launching Headless Mode");
+			return playwright.chromium().launch(
+					new BrowserType.LaunchOptions()
+					.setHeadless(true));
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
+	
+	public void navigate(Browser browser, String url) {
+		page = browser.newPage();
+		page.navigate(url);
+		log.info("Navigate to: " + url);
+	}
+	
+	@AfterMethod
+	public void quit() {
+		browser.close();
+		page.close();
+		playwright.close();
+	}
+}
